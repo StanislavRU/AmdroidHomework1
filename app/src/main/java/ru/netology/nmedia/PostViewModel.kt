5fd5633ce07bbfdab4dc0164133a11ec0313bd61
StanislavRu <stanislav.com@inbox.ru>
2021-10-20 23:44:50 +0500

    Add CRUD

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -2,14 +2,21 @@ package ru.netology.nmedia
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemory
+
+private val empty = Post()
 
 class PostViewModel : ViewModel() {
     // TODO упрощенный вариант
     private val repository: PostRepository = PostRepositoryInMemory()
     val data: LiveData<List<Post>> = repository.data
+    private val _edited = MutableLiveData(empty)
+    val edited: LiveData<Post>
+        get() = _edited
+
     fun likeById(id: Long) {
         repository.likeById(id)
     }
@@ -20,5 +27,27 @@ class PostViewModel : ViewModel() {
 
     fun viewById(id: Long) {
         repository.viewsById(id)
+    }
+
+    fun edit(post: Post) {
+        _edited.value = post
+    }
+
+    fun removeById(id: Long) {
+        repository.removeById(id)
+    }
+
+    fun changeContent(content: String) {
+        _edited.value = _edited.value?.copy(content = content)
+    }
+    fun save() {
+        _edited.value?.also {
+            repository.save(it)
+            _edited.value = empty
+        }
+    }
+
+    fun cancelButton() {
+        _edited.value = empty
     }
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemory.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemory.kt
@@ -5,10 +5,12 @@ import ru.netology.nmedia.dto.Post
 
 class PostRepositoryInMemory : PostRepository {
 
-    private companion object {
-        val defaultPosts = listOf(
+    private var postId = 1L
+
+    private val defaultPosts =
+        listOf(
             Post(
-                id = 9,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Освоение новой профессии — это не только открывающиеся возможности и перспективы, но и настоящий вызов самому себе. Приходится выходить из зоны комфорта и перестраивать привычный образ жизни: менять распорядок дня, искать время для занятий, быть готовым к возможным неудачам в начале пути. В блоге рассказали, как избежать стресса на курсах профпереподготовки → http://netolo.gy/fPD",
                 published = "23 сентября в 10:12",
@@ -17,7 +19,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueRepost = 12
             ),
             Post(
-                id = 8,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Делиться впечатлениями о любимых фильмах легко, а что если рассказать так, чтобы все заскучали \uD83D\uDE34\n",
                 published = "22 сентября в 10:14",
@@ -26,7 +28,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueRepost = 999
             ),
             Post(
-                id = 7,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Таймбоксинг — отличный способ навести порядок в своём календаре и разобраться с делами, которые долго откладывали на потом. Его главный принцип — на каждое дело заранее выделяется определённый отрезок времени. В это время вы работаете только над одной задачей, не переключаясь на другие. Собрали советы, которые помогут внедрить таймбоксинг \uD83D\uDC47\uD83C\uDFFB",
                 published = "22 сентября в 10:12",
@@ -35,7 +37,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueRepost = 555
             ),
             Post(
-                id = 6,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "\uD83D\uDE80 24 сентября стартует новый поток бесплатного курса «Диджитал-старт: первый шаг к востребованной профессии» — за две недели вы попробуете себя в разных профессиях и определите, что подходит именно вам → http://netolo.gy/fQ",
                 published = "21 сентября в 10:12",
@@ -44,7 +46,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueRepost = 38
             ),
             Post(
-                id = 5,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Диджитал давно стал частью нашей жизни: мы общаемся в социальных сетях и мессенджерах, заказываем еду, такси и оплачиваем счета через приложения.",
                 published = "20 сентября в 10:14",
@@ -53,7 +55,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueRepost = 100
             ),
             Post(
-                id = 4,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Большая афиша мероприятий осени: конференции, выставки и хакатоны для жителей Москвы, Ульяновска и Новосибирска \uD83D\uDE09",
                 published = "19 сентября в 14:12",
@@ -61,7 +63,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueLiked = 0
             ),
             Post(
-                id = 3,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Языков программирования много, и выбрать какой-то один бывает нелегко. Собрали подборку статей, которая поможет вам начать, если вы остановили свой выбор на JavaScript.",
                 published = "19 сентября в 10:24",
@@ -69,7 +71,7 @@ class PostRepositoryInMemory : PostRepository {
                 valueLiked = 10
             ),
             Post(
-                id = 2,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Знаний хватит на всех: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях \uD83D\uDC47",
                 published = "18 сентября в 10:12",
@@ -77,18 +79,16 @@ class PostRepositoryInMemory : PostRepository {
                 valueLiked = 999
             ),
             Post(
-                id = 1,
+                id = postId++,
                 author = "Нетология. Университет интернет-профессий будущего",
                 content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
                 published = "21 мая в 18:36",
                 likedByMe = false,
                 valueLiked = 123
             ),
-        )
-    }
+        ).reversed()
 
     override val data = MutableLiveData(defaultPosts)
-
 
     override fun likeById(id: Long) {
         val currentPosts: List<Post> = data.value ?: return
@@ -122,5 +122,25 @@ class PostRepositoryInMemory : PostRepository {
             ) else it
         }
         data.value = result
+    }
+
+    override fun removeById(id: Long) {
+        data.value = data.value?.filter {it.id != id}
+    }
+
+    override fun save(post: Post) {
+        if (post.id == 0L) {
+            val newPost = post.copy(id = postId++)
+            data.value = listOf(newPost) + data.value.orEmpty()
+            return
+        }
+
+        data.value = data.value?.map {
+            if (it.id == post.id) {
+                it.copy(content = post.content, id = postId++)
+            } else {
+                it
+            }
+        }
     }
 }
